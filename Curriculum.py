bac6190424d94fb72ed1b9a36ec0cbb4f6366040
Austin Starks <as2779@cornell.edu>
2020-03-22 19:01:37 -0400

    Edit curriculum for typos and clarification

--- Curriculum.py
+++ Curriculum.py
@@ -3,6 +3,8 @@ sys.path.insert(1, "Implementations")
 import LinkedListTest
 import StackTest
 import QueueTest
+import TreeTest
+import TrieTest
 
 
 # Curriculum
@@ -12,13 +14,12 @@ import QueueTest
 # of data; each data point being identified by an index
 # -	Big-O for insert: O(n)
 # -	Big-O for delete: O(n)
-# -	Big-O for lookup: O(1)
+# -	Big-O for lookup: O(1) (O(n) to find the data point, but O(1) if you know index)
 # -	Example use: To implement a matrix
-
 
 # •	Linked List Description: A collection of data whose order is given by the
 # element it points to. Each node point contains data and the next value
-# -	Big-O for insert: O(1) (to insert; looking up is still O(1)
+# -	Big-O for insert: O(1) (to insert; looking up is still O(n) )
 # -	Big-O for delete: O(1)
 # -	Big-O for lookup: O(n)
 # -	Example use: An image viewer that loads the next and previous image or a
@@ -26,39 +27,42 @@ import QueueTest
 #  to implement a stack/queue (doubly linked).
 # -	Implementation: LinkedList.py
 
-
 # •	Stack Description: A data type that holds a collection of elements that can
 # only be accessed by adding and removing from the top of the stack (LIFO). 
 # They support push and pop. A stack is used for DFS.
 # -	Big-O for insert: O(1)
 # -	Big-O for delete: O(1)
 # -	Big-O for lookup: O(n)
-# -	Implementation link (include pop and push): Stack.py
 # -	Example use: DFS
-
+# -	Implementation link (include pop and push): Stack.py
 
 # •	Queue Description: A data type that holds a collection of elements that can 
 #   only be accessed by adding from the top and removing from the buttom (FIFO)
 # -	Big-O for insert: O(1)
 # -	Big-O for delete: O(1)
 # -	Big-O for lookup: O(n)
-# -	Implementation link (include queue and enqueue): Queue.py
 # -	Example use: BFS
+# -	Implementation link (include queue and enqueue): Queue.py
 
+# •	Tree Description: An undirected acyclic graph. A tree is a node that has 
+#   children that are also nodes.
+# -	Tree height definition: The length of the longest downward path to a leaf 
+#                           from a root node
+# -	Tree depth definition: The length of the path to its root
+# - Implementation link: Tree.py
 
-# •	Tree Description:
-# -	Tree height definition:
-# -	Tree depth definition:
-# -	Big-O for insert:
-# -	Big-O for delete:
-# -	Big-O for lookup:
-# -	Implementation link:
+# •	Trie Description: A tree that can effeciently store words. It allows 
+#   one to effeciently store words, especially words in which a lot of them 
+#   start with the same letter.
+# -	Big-O for insert: O(n) (length of the key you're storing)
+# -	Big-O for delete: O(n) (faster if you want to delete all words with a certain
+#                     prefix)
+# -	Big-O for lookup: O(n)
+# - Example use: creating an operating system; the folders are nodes and the 
+# - the data is a list of files. Or storing large genomes in whih you don't have 
+#   a lot of space.
+# -	Implementation link: Trie.py
 
-# •	Trie Description:
-# -	Big-O for insert:
-# -	Big-O for delete:
-# -	Big-O for lookup:
-# -	Implementation link:
 
 # •	Binary Search Tree Description:
 # -	Big-O for insert:
@@ -132,4 +136,6 @@ if __name__ == '__main__':
     LinkedListTest.tests()
     StackTest.tests()
     QueueTest.tests()
+    TreeTest.tests()
+    TrieTest.tests()
     
